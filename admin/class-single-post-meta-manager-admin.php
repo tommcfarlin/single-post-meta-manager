aa34aee2baa793a8184e3ea287d63234fb70954f
Tom McFarlin <tom@tommcfarlin.com>
2014-05-15 09:09:21 -0400

    adding the stylesheets and the metabox for the dashboard

--- admin/class-single-post-meta-manager-admin.php
+++ admin/class-single-post-meta-manager-admin.php
@@ -10,10 +10,31 @@ class Single_Post_Meta_Manager_Admin {
 
 	public function enqueue_styles() {
 
+		wp_enqueue_style(
+			'single-post-meta-manager-admin',
+			plugin_dir_url( __FILE__ ) . 'css/single-post-meta-manager-admin.css',
+			array(),
+			$this->version,
+			FALSE
+		);
+
 	}
 
 	public function add_meta_box() {
 
+		add_meta_box(
+			'single-post-meta-manager-admin',
+			'Single Post Meta Manager',
+			array( $this, 'render_meta_box' ),
+			'post',
+			'normal',
+			'core'
+		);
+
+	}
+
+	public function render_meta_box() {
+		require_once plugin_dir_path( __FILE__ ) . 'partials/single-post-meta-manager.php';
 	}
 
 }
